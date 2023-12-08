07055dea10e4ef7fbd35a297849892897192398f
zakaWAC <129171507+zakaWAC@users.noreply.github.com>
2023-12-08 19:05:27 +0100

    finished project

---
+++ app/src/main/java/com/example/authentification/LoginActivity.kt
@@ -0,0 +1,44 @@
+package com.example.authentification
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.authentification.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+
+        binding.loginButton.setOnClickListener {
+            val loginUsername = binding.loginUsername.text.toString()
+            val loginPassword = binding.loginPassword.text.toString()
+            loginDatabase(loginUsername, loginPassword)
+        }
+        binding.signupRedirect.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun loginDatabase(username: String, password: String){
+        val userExists = databaseHelper.readUser(username, password)
+        if (userExists){
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/authentification/SignupActivity.kt
@@ -0,0 +1,45 @@
+package com.example.authentification
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.authentification.databinding.ActivitySignupBinding
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+
+        binding.signupButton.setOnClickListener {
+            val signupUsername = binding.signupUsername.text.toString()
+            val signupPassword = binding.signupPassword.text.toString()
+            signupDatabase(signupUsername, signupPassword)
+        }
+
+        binding.loginRedirect.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun signupDatabase(username: String, password: String){
+        val insertedRowId = databaseHelper.insertUser(username, password)
+        if (insertedRowId != -1L){
+            Toast.makeText(this, "Signup Successful", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else {
+            Toast.makeText(this, "Signup Failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
